03bff87265e60ab0da4c79edcb4261f90eaa33e6
Aleksandr Kruglov <alkruglov777@gmail.com>
2018-05-23 09:48:36 +0300

    Add full jpa config settings but
    Done #0006

--- src/main/java/ua/rafael/jpa/App.java
+++ src/main/java/ua/rafael/jpa/App.java
@@ -1,8 +1,14 @@
 package ua.rafael.jpa;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import ua.rafael.jpa.config.JpaConfig;
+
 public class App {
 
   public static void main(String[] args) {
-
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(JpaConfig.class);
+    applicationContext.getBean(LocalContainerEntityManagerFactoryBean.class);
   }
 }
--- src/main/java/ua/rafael/jpa/config/JpaConfig.java
+++ src/main/java/ua/rafael/jpa/config/JpaConfig.java
@@ -1,19 +1,19 @@
 package ua.rafael.jpa.config;
 
+import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-import javax.transaction.TransactionManager;
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.AbstractEntityManagerFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -25,6 +25,12 @@ public class JpaConfig {
   public static String URL = "hibernate.connection.url";
   public static String USER_NAME = "hibernate.connection.username";
   public static String PASSWORD = "hibernate.connection.password";
+
+  public static String DIALECT = "hibernate.dialect";
+  public static String MAX_FETCH_DEPTH = "hibernate.max_fetch_depth";
+  public static String FETCH_SIZE = "hibernate.jdbc.fetch_size";
+  public static String BATCH_SIZE = "hibernate.jdbc.batch_size";
+  public static String SHOW_SQL = "hibernate.show_sql";
 
   @Autowired
   private Environment env;
@@ -40,15 +46,37 @@ public class JpaConfig {
 
   @Bean
   @Autowired
-  public JpaTransactionManager transactionManager( managerFactoryBean) {
+  public JpaTransactionManager transactionManager(EntityManagerFactory managerFactoryBean) {
     JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
-    jpaTransactionManager.set;
-    return null;
+    jpaTransactionManager.setEntityManagerFactory(managerFactoryBean);;
+    return jpaTransactionManager;
   }
-  
+
   @Bean
-  public AbstractEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-    LocalContainerEntityManagerFactoryBean entityManagerFactoryBean =  new LocalContainerEntityManagerFactoryBean();
-    return entityManagerFactory;
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    LocalContainerEntityManagerFactoryBean entityManagerFactoryBean =
+        new LocalContainerEntityManagerFactoryBean();
+    entityManagerFactoryBean.setDataSource(dataSource());
+    entityManagerFactoryBean.setPackagesToScan(new String[] {"ua.rafael.jpa.model"});
+    entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+    entityManagerFactoryBean.setJpaProperties(jpaProperties());
+    return entityManagerFactoryBean;
+  }
+
+  @Bean
+  public PersistenceExceptionTranslationPostProcessor exceptionTranslator() {
+    return new PersistenceExceptionTranslationPostProcessor();
+  }
+
+  private Properties jpaProperties() {
+    return new Properties() {
+      {
+        setProperty(DIALECT, env.getProperty(DIALECT));
+        setProperty(MAX_FETCH_DEPTH, env.getProperty(MAX_FETCH_DEPTH));
+        setProperty(FETCH_SIZE, env.getProperty(FETCH_SIZE));
+        setProperty(BATCH_SIZE, env.getProperty(BATCH_SIZE));
+        setProperty(SHOW_SQL, env.getProperty(SHOW_SQL));
+      }
+    };
   }
 }
